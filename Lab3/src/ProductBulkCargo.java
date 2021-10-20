a12857780e2d3b3f15f1055d89e3d4fde1bee8a6
Pocik-lab <79202142+Pocik-lab@users.noreply.github.com>
2021-10-20 21:29:47 +0300

    Update ProductBulkCargo.java

--- Lab3/src/ProductBulkCargo.java
+++ Lab3/src/ProductBulkCargo.java
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
 package Lab3;
 
 import static Lab3.CheckFunctions.*;
 import java.io.*;
 
-<<<<<<< HEAD
 /** 
  * Класс продукта со свойствами <b>bulk_cargo_type</b> и <b>bulk_cargo_density</b>.
  * @autor Тегин Александр
@@ -28,65 +19,42 @@ public class ProductBulkCargo extends Product implements Serializable
      * Процедура определения типа сыпучего груза {@link ProductBulkCargo#bulk_cargo_type}
      * @param bulk_cargo_type - тип сыпучего груза
      */
-=======
-/**
- *
- * @author aleksandrtegin
- */
-public class ProductBulkCargo extends Product implements Serializable{
-    
-    private String bulk_cargo_type;
-    private double bulk_cargo_density;
-    
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
     public void setBulkCargoType(String bulk_cargo_value)
     {
         this.bulk_cargo_type = bulk_cargo_value;
     }
-    
-<<<<<<< HEAD
+
     /**
      * Процедура определения плотности груза {@link ProductBulkCargo#bulk_cargo_density}
      * @param bulk_cargo_density - плотность груза
      */
-=======
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
     public void setBulkCargoDensity(double bulk_cargo_density_value)
     {   
         this.bulk_cargo_density = bulk_cargo_density_value;
     }
-    
-<<<<<<< HEAD
+
     /**
      * Функция получения значения поля {@link ProductBulkCargo#bulk_cargo_type}
      * @return возвращает тип сыпучего груза
      */
-=======
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
     public String getBulkCargoType()
     {   
         return bulk_cargo_type;
     }
-    
-<<<<<<< HEAD
+
     /**
      * Функция получения значения поля {@link ProductBulkCargo#bulk_cargo_density}
      * @return возвращает плотность груза
      */
-=======
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
     public double getBulkCargoDensity()
     {
         return bulk_cargo_density;
     }
-    
-<<<<<<< HEAD
+
     /** 
      * Конструктор - создание нового объекта
      * @see ProductBulkCargo#ProductBulkCargo(int, double, String,  double)
      */
-=======
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
     protected ProductBulkCargo ()
     {
         super();
@@ -95,8 +63,7 @@ public class ProductBulkCargo extends Product implements Serializable{
         System.out.println("\nВведите плотность материала:\n");
         bulk_cargo_density = DoubleCheck();
     }
-    
-<<<<<<< HEAD
+
     /** 
      * Конструктор - создание нового объекта с определенными значениями
      * @param spec_id_v - ID продукта
@@ -108,16 +75,10 @@ public class ProductBulkCargo extends Product implements Serializable{
     protected ProductBulkCargo (int spec_id_v,double prod_w_v, String bulc_t_v, double bulc_d_v)
     {
         super(spec_id_v, prod_w_v);
-=======
-    protected ProductBulkCargo (int count_v,double prod_w_v, String bulc_t_v, double bulc_d_v)
-    {
-        super(count_v, prod_w_v);
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
         this.bulk_cargo_type = bulc_t_v;
         this.bulk_cargo_density = bulc_d_v;
     }
-    
-<<<<<<< HEAD
+
     /**
      * Процедура словесного описания данного продукта
      */
@@ -127,11 +88,3 @@ public class ProductBulkCargo extends Product implements Serializable{
         return "Сыпучий груз\nID товара: " + super.getSpecialID()+ " Вес товара: " + super.getProdWeight() + "\n" + "Тип сыпучего груза: " + bulk_cargo_type + " Вес груза (кг): " + bulk_cargo_density + "\n";
     }
 }
-=======
-    @Override
-    public String toString()
-    {
-        return "Сыпучий груз\nКоличество товара: " + super.getCount() + " Вес товара: " + super.getProdWeight() + "\n" + "Тип сыпучего груза: " + bulk_cargo_type + " Вес груза (кг): " + bulk_cargo_density + "\n";
-    }
-}
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
