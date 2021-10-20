6155dbcd76c747818a3dce1f6a051d60c6a04b91
Pocik-lab <79202142+Pocik-lab@users.noreply.github.com>
2021-10-20 18:36:29 +0300

    Update CheckFunctions.java

--- Lab3/src/CheckFunctions.java
+++ Lab3/src/CheckFunctions.java
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
 package Lab3;
 
 import java.util.*;
 
-<<<<<<< HEAD
 /** 
  * Класс спец функций на проверку типажей.
  * @autor Тегин Александр
@@ -22,27 +13,16 @@ public class CheckFunctions {
      * Процедура проверки на int
      * @return number - корректный int
      */
-=======
-/**
- *
- * @author aleksandrtegin
- */
-public class CheckFunctions {
-    
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
     public static int IntCheck()
     {
         int number = 0;
         boolean CorrectValues;
         Scanner sc = new Scanner(System.in);
-        
-<<<<<<< HEAD
+
         /**
          * В данном блоке проходит проверка на корректный ввод данных
          * @exception InputMismatchException введен некорректный тип данных 
          */
-=======
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
         do
         {
             try 
@@ -62,27 +42,21 @@ public class CheckFunctions {
         
         return number;
     }
-    
-<<<<<<< HEAD
+
     /**
      * Процедура проверки на double
      * @return number - корректный double
      */
-=======
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
     public static double DoubleCheck()
     {
         double number = 0;
         boolean CorrectValues;
         Scanner sc = new Scanner(System.in);
-        
-<<<<<<< HEAD
+
         /**
          * В данном блоке проходит проверка на корректный ввод данных
          * @exception InputMismatchException введен некорректный тип данных 
          */
-=======
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
         do
         {
             try 
@@ -103,26 +77,20 @@ public class CheckFunctions {
         return number;
     }
     
-<<<<<<< HEAD
     /**
      * Процедура проверки на string
      * @return number - корректный string
      */
-=======
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
     public static String StringCheck()
     {
         String s = "";
         boolean CorrectValues;
         Scanner sc = new Scanner(System.in);
-        
-<<<<<<< HEAD
+
         /**
          * В данном блоке проходит проверка на корректный ввод данных
          * @exception InputMismatchException введен некорректный тип данных 
          */
-=======
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
         do
         {
             try 
@@ -142,8 +110,4 @@ public class CheckFunctions {
         
         return s;
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 3a9d30eac2934ed5cb1bbda21a4db3d6fa387bed
